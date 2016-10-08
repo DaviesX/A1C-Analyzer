f69bcb9b189987b857918a37c27d6f1388a98d4d
Zhaonian Luan <zhaonial@uci.edu>
2016-10-07 22:32:22 -0700

    Created a UI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <iostream>
+using namespace std;
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +14,24 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_start_clicked()
+{
+    cout << "start clicked!" << endl;
+}
+
+void MainWindow::open_lab()
+{
+
+}
+
+void MainWindow::on_lab_triggered()
+{
+    cout << "lab triggered!" << endl;
+}
+
+void MainWindow::on_order_triggered()
+{
+    cout << "order triggered!" << endl;
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,15 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+
+    void on_start_clicked();
+    void open_lab();
+
+    void on_lab_triggered();
+
+    void on_order_triggered();
+
 private:
     Ui::MainWindow *ui;
 };
